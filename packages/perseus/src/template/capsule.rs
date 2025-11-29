3ddc1532a813c958146c0f003a79844f99655d7a
afidegnum <afidegnum@yahoo.com>
2025-11-29 22:23:50 +0000

    fix: update create_root to create_child_scope for Sycamore 0.9.2
    - Replace deprecated create_root with create_child_scope in capsule template
    - Part of Sycamore 0.8 to 0.9.2 migration

--- packages/perseus/src/template/capsule.rs
+++ packages/perseus/src/template/capsule.rs
@@ -124,7 +124,7 @@ impl<P: Clone + 'static> Capsule<P> {
         // Wipe the template's view function to make sure the errors aren't obscenely
         // weird
         template_inner.view = Box::new(|_, _, _| {
-            let _disposer = create_root(|| {});
+            let _disposer = create_child_scope(|| {});
             Ok((View::new(), _disposer))
         });
         CapsuleInner {
