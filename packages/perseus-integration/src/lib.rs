bf6bbf2a05328492828bc664712ada36c0710bba
afidegnum <afidegnum@yahoo.com>
2025-12-11 23:02:56 +0000

    fix: resolve server integration feature conflicts
    - Remove default feature from perseus-integration to avoid
      feature unification conflicts in workspace builds
    - Add compile-time checks to ensure only one server integration
      is enabled at a time
    - Fixes E0252 error where multiple integrations export dflt_server

--- packages/perseus-integration/src/lib.rs
+++ packages/perseus-integration/src/lib.rs
@@ -1,5 +1,19 @@
 #![cfg(engine)]
 
+// Ensure only one server integration is enabled at a time
+#[cfg(all(feature = "actix-web", feature = "axum"))]
+compile_error!("Only one server integration feature can be enabled at a time");
+#[cfg(all(feature = "actix-web", feature = "rocket"))]
+compile_error!("Only one server integration feature can be enabled at a time");
+#[cfg(all(feature = "actix-web", feature = "warp"))]
+compile_error!("Only one server integration feature can be enabled at a time");
+#[cfg(all(feature = "axum", feature = "rocket"))]
+compile_error!("Only one server integration feature can be enabled at a time");
+#[cfg(all(feature = "axum", feature = "warp"))]
+compile_error!("Only one server integration feature can be enabled at a time");
+#[cfg(all(feature = "rocket", feature = "warp"))]
+compile_error!("Only one server integration feature can be enabled at a time");
+
 #[cfg(feature = "actix-web")]
 pub use perseus_actix_web::dflt_server;
 #[cfg(feature = "axum")]
